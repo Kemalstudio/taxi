a54352ee076c9908d7a25b92bc60417afcff9179
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-08 19:35:59 +0500

    2.1

---
+++ admin-dashboard/src/lib/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const TOKEN_KEY = "taxi_admin_token";
+
+export const tokenStore = {
+  get: () => localStorage.getItem(TOKEN_KEY),
+  set: (token: string) => localStorage.setItem(TOKEN_KEY, token),
+  clear: () => localStorage.removeItem(TOKEN_KEY),
+};
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080",
+});
+
+api.interceptors.request.use((config) => {
+  const token = tokenStore.get();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
