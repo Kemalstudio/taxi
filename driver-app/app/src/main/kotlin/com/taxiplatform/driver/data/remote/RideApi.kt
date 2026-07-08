aac9c13ac71b0bffe13f24856b362cda5aeea853
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-08 18:29:59 +0500

    1,63

---
+++ driver-app/app/src/main/kotlin/com/taxiplatform/driver/data/remote/RideApi.kt
@@ -0,0 +1,23 @@
+package com.taxiplatform.driver.data.remote
+
+import com.taxiplatform.driver.data.remote.dto.RideResponseDto
+import retrofit2.Response
+import retrofit2.http.Path
+import retrofit2.http.POST
+
+interface RideApi {
+	@POST("rides/{id}/accept")
+	suspend fun accept(@Path("id") rideId: String): RideResponseDto
+
+	@POST("rides/{id}/reject")
+	suspend fun reject(@Path("id") rideId: String): Response<Unit>
+
+	@POST("rides/{id}/arrive")
+	suspend fun arrive(@Path("id") rideId: String): RideResponseDto
+
+	@POST("rides/{id}/start")
+	suspend fun start(@Path("id") rideId: String): RideResponseDto
+
+	@POST("rides/{id}/complete")
+	suspend fun complete(@Path("id") rideId: String): RideResponseDto
+}
