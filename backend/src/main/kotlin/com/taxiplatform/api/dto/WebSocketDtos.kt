6cc43310363e2e39cbe58df53b61ab7262fe2bc1
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-08 16:56:42 +0500

    1.37

---
+++ backend/src/main/kotlin/com/taxiplatform/api/dto/WebSocketDtos.kt
@@ -0,0 +1,17 @@
+package com.taxiplatform.api.dto
+
+import java.util.UUID
+
+data class GeoPointDto(val lat: Double, val lng: Double)
+
+data class RideOfferMessage(
+	val rideId: UUID,
+	val pickup: GeoPointDto,
+	val dropoff: GeoPointDto,
+)
+
+data class RideStatusMessage(
+	val rideId: UUID,
+	val status: String,
+	val driverId: UUID?,
+)
