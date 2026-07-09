d57d316f2c1e2c6891cf85c1bc0b80449d2e4f69
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-09 12:21:23 +0500

    3.11

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "leaflet/dist/leaflet.css";
+import "maplibre-gl/dist/maplibre-gl.css";
 import "@fontsource/plus-jakarta-sans/400.css";
 import "@fontsource/plus-jakarta-sans/500.css";
 import "@fontsource/plus-jakarta-sans/600.css";
