877bab46c7639484565f0fd6bb17a508e1dfc4de
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-09 11:31:49 +0500

    2.97

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -8,9 +8,15 @@ import "@fontsource/plus-jakarta-sans/700.css";
 import "@fontsource/plus-jakarta-sans/800.css";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "./theme";
+import { I18nProvider } from "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <I18nProvider>
+        <App />
+      </I18nProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
