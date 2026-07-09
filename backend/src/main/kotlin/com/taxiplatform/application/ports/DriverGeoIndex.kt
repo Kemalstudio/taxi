c5e91deb40208d2fdd9b411aa407342461c04dee
Kemal Altayev <kemalatayew913@gmail.com>
2026-07-09 09:36:49 +0500

    2.9

--- backend/src/main/kotlin/com/taxiplatform/application/ports/DriverGeoIndex.kt
+++ backend/src/main/kotlin/com/taxiplatform/application/ports/DriverGeoIndex.kt
@@ -1,6 +1,7 @@
 package com.taxiplatform.application.ports
 
 import com.taxiplatform.domain.geo.DriverCandidate
+import com.taxiplatform.domain.geo.DriverLocation
 import com.taxiplatform.domain.geo.GeoPoint
 import java.util.UUID
 
@@ -10,4 +11,7 @@ interface DriverGeoIndex {
 
 	/** Returns online drivers within [radiusKm] of [point], nearest first. */
 	fun findNearby(point: GeoPoint, radiusKm: Double): List<DriverCandidate>
+
+	/** All drivers currently in the live geo index (used by the admin live map). */
+	fun findAllOnline(): List<DriverLocation>
 }
